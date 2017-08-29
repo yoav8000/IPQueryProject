8f31a3af9347e500d4689af27204ba56356ff967
yoav gomberg <yoav gomberg@DESKTOP-UQ08A7L>
2017-08-29 23:37:33 +0300

    finished the project

--- QueryIPInfo.py
+++ QueryIPInfo.py
@@ -4,23 +4,28 @@ from IPInfo import *
 from MarkdownFormatter import *
 import sys
 
+PREFIX = 'http://ipinfo.io/'
+SUFFIX = '/json'  # forces the server to return a json file.
+
+
+def write_markdown(formatted_info, ip):
+    markdown_file = open('{}_information.md'.format(ip), "w")
+    markdown_file.write(formatted_info)
+    markdown_file.close()
+
 
 def query_ip():
     if len(sys.argv) != 2:
-        print "Usage: QueryIPInfo.py IP-ADDRESS(for exanple: 8.8.8.8) "
+        print "Usage: QueryIPInfo.py IP-ADDRESS (e.g 8.8.8.8)"
         sys.exit(1)
     else:
         ip_address = sys.argv[1]
-        h = RestCommunicator()
-        prefix = 'https://ipinfo.io/'
-        suffix = '/json'  # forces the server to return a json file.
-        raw_info = h.send_get_request(prefix, ip_address, suffix)
+        communicator = RestCommunicator()
+        raw_info = communicator.send_get_request(PREFIX, ip_address, SUFFIX)
         ip_info = IPInfo(JsonParser(), raw_info)
         md_formatter = MarkdownFormatter(ip_info)
         formatted_info = md_formatter.form()
-        Markdown_file = open("IP Information.md", "w")
-        Markdown_file.write(formatted_info)
-        Markdown_file.close()
+        write_markdown(formatted_info, ip_address)
 
 
 if __name__ == '__main__':
